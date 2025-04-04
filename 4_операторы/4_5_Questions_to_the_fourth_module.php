8a415aa8342619608b32e5d589bd5c3fb58e8464
yourun92 <ostanin.iurii@gmail.com>
2025-04-04 10:50:34 +0300

    Some changes

---
+++ 4_операторы/4_5_Questions_to_the_fourth_module.php
@@ -0,0 +1,11 @@
+<?php
+    $a = readline();
+    $b = readline();
+    $c = readline();
+    $d = readline();
+    $res = $a + $b + $c;
+    $res *= $d;
+    $res %= 10;
+    $res /= 5;
+    echo $res;
+?>
